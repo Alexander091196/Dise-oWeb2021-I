fb696bb4a156b83375138e0dd0cf4640d0bb3c25
Alexander <seclen49@gmail.com>
2021-11-10 21:59:16 -0500

    clase

--- sesion21/js/agendaV1.js
+++ sesion21/js/agendaV1.js
@@ -1,12 +1,14 @@
 function guardardatos(){
-localStorage.nombre=document.getElementById("nombre").value;
-localStorage.movil=document.getElementById("movil").value;
+localStorage.nombre = document.getElementById("nombre").value;
+localStorage.movil = document.getElementById("movil").value;
 }
+
 function recuperardatos(){
     if((localStorage.nombre!=undefined) && (localStorage.movil!=undefined)){
-    document.getElementById("datos").innerHTML="Nombre: " + localStorage.nombre + "<br> Num. Celular: " + localStorage.movil;       
+        /* innerHTML = escribir directo del parrafo */
+    document.getElementById("datos").innerHTML = "Nombre: " + localStorage.nombre + "<br> Num. Celular: " + localStorage.movil;       
     }
     else{
-    document.getElementById("datos").innerHTML="No haz ingresado tus datos en la agenda";
+    document.getElementById("datos").innerHTML = "No haz ingresado tus datos en la agenda";
     }
 }
--- sesion21/js/agendaV3.js
+++ sesion21/js/agendaV3.js
@@ -2,51 +2,74 @@ function guardarDato(){
     var nombre=document.getElementById("nombre").value;
     var movil=document.getElementById("movil").value;
     var email=document.getElementById("email").value;
-    localStorage.setItem("nombre",nombre);
-    localStorage.setItem("movil",movil);
-    localStorage.setItem("email",email);
+
+    /* Const = los datos no pueden remplazarce - pero tambien es amplia */
+    const datos = {
+        'movil': movil,
+        'email': email,
+    }
+     /* Lo convertimos a string los datos */
+    localStorage.setItem(nombre, JSON.stringify(datos));
+
     document.getElementById("nombre").value="";
     document.getElementById("movil").value="";
     document.getElementById("email").value="";
     actualizarDatos();
 }
+
 function recuperarDato(){
     var nombre=document.getElementById("nombre").value;
-    localStorage.getItem(nombre);
-    document.getElementById("movil").value=localStorage.getItem(nombre);
-    document.getElementById("email").value=localStorage.getItem(nombre);
+    /* En contramos el nombre y almacenamos los datos personales en "datos" */
+    let datos = localStorage.getItem(nombre);
+    /* Separando los valores que capturamos en esta variable */
+    datos = JSON.parse(datos);
+
+    document.getElementById("movil").value = datos.movil;
+    document.getElementById("email").value = datos.email;
 }
+
 function eliminarDato(){
     var nombre= document.getElementById("nombre").value;
     localStorage.removeItem(nombre);
     actualizarDatos();
 }
+
 function eliminarTodo(){
     localStorage.clear();
     actualizarDatos();
 }
+
 function actualizarDatos(){
     var registro="";
-    if(localStorage.length==0){
+    if(localStorage.length === 0){
         registro +='<li>Vacio</li>';
-    } 
-    else{ 
-        console.log(localStorage);      
-        for(var i=0; i<=localStorage.length -1; i++){
+    } else{ 
+        for(var i=0; i <= localStorage.length -1; i++){
             var key=localStorage.key(i);
-            switch(i){
-                case 0:
-                    registro+= '<span class="nom">' + localStorage.getItem('nombre') + '</span>';
-                    break;
-                case 1:
-                    registro+= '<span class="nom">' + localStorage.getItem('movil') + '</span>';
-                    break;
-                case 2:
-                    registro+= '<span class="nom">' + localStorage.getItem('email') + '</span>'; 
-                    break;                                     
-            }
-           
-        }   
+            let datos = localStorage.getItem(key);
+            datos = JSON.parse(datos);
+
+            registro += `<li> <span class="nom"> ${key} </span>
+                              <span class="nom"> ${datos.movil}  </span> 
+                              <span class="nomemail"> ${datos.email}  </span>
+                        </li><br>`;  
+                    }
     }
     document.getElementById('contactos').innerHTML=registro;
 }
+
+/* Valindado datos para solo letras */
+function sololetras(evt){
+    var charCode = (evt.charCode);
+    if((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)){
+        document.getElementById("aviso").innerHTML = "";
+        return true;
+    } else{
+        // alert("ingrese solo letras");
+        // mensaje de aletar abajo del dato
+        document.getElementById("aviso").innerHTML = 'Ingrese solo letras';
+        return false;
+    }
+}
+
+
